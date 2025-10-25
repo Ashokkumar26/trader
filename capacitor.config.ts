0d1d35340ac37cdfa9cbf3f67f9380c77aad031f
Ashokkumar <ashokkumar@Ashokkumars-MacBook-Air.local>
2025-10-25 16:42:07 +0530

    Config update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'trader',
-  webDir: 'out',
+  webDir: '.next',
   bundledWebRuntime: false
 };
 
